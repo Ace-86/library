42ea0f00c22e57ddd32426a992dd90cae29c4d1b
Ace <kronicrespawn@gmail.com>
2022-07-17 22:09:03 -0500

    modal added

--- script.js
+++ script.js
@@ -1,48 +1,118 @@
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    // this.read = read;
-    // this.info = function () {
-    //      return 'The book title: ' + title + ' by ' + author + '. The book is ' + pages +' pages long. book completed: ' + read ;
-    // }
+let modal = document.getElementById("modalForm");
+let btn = document.getElementById("addBtn");
+let span = document.getElementsByClassName("exit")[0];
+
+btn.onclick = function() {
+    modal.style.display = "block";
+
 }
 
-Book.prototype.sayTitle = function () {
-    console.log(this.title)
+span.onclick = function () {
+    modal.style.display = "none";
 }
 
-const book = {
-    read : false,
-    info :function () {
-        return 'The book title: ' + title + ' by ' + author + '. The book is ' + pages +' pages long. book completed: ' + read ;
-   }
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
 
-const book1 = new Book('IT', 'Steven King', '978', 'no');
-const book2 = new Book('Nice Guy', 'Dean Koontz', '648', 'yes')
-const book3 = new Book('Danger Zone', 'Aiden Finnigan', '540', 'no')
-const mybook = Object.create(book);
-// const person = {
-//     isHuman: false,
-//     printIntroduction: function() {
-//       console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
-//     }
-//   };
 
 
 
-//   const Ace = Object.create(person);
 
-// Ace.name = 'Aaron';
-// Ace.isHuman = true;
 
-// Ace.printIntroduction();
 
-mybook.title = 'Ace Stories';
-mybook.author = 'Aaron Griffin';
-mybook.pages = '300';
-mybook.read = 'yes';
 
-console.log(mybook)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function Book(title, author, pages, read) {
+//     this.title = title;
+//     this.author = author;
+//     this.pages = pages;
+//     // this.read = read;
+//     // this.info = function () {
+//     //      return 'The book title: ' + title + ' by ' + author + '. The book is ' + pages +' pages long. book completed: ' + read ;
+//     // }
+// }
+
+// Book.prototype.sayTitle = function () {
+//     console.log(this.title)
+// }
+
+// const book = {
+//     read : false,
+//     info :function () {
+//         return 'The book title: ' + title + ' by ' + author + '. The book is ' + pages +' pages long. book completed: ' + read ;
+//    }
+// }
+
+
+// const book1 = new Book('IT', 'Steven King', '978', 'no');
+// const book2 = new Book('Nice Guy', 'Dean Koontz', '648', 'yes')
+// const book3 = new Book('Danger Zone', 'Aiden Finnigan', '540', 'no')
+// const mybook = Object.create(book);
+// // const person = {
+// //     isHuman: false,
+// //     printIntroduction: function() {
+// //       console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
+// //     }
+// //   };
+
+
+
+// //   const Ace = Object.create(person);
+
+// // Ace.name = 'Aaron';
+// // Ace.isHuman = true;
+
+// // Ace.printIntroduction();
+
+// mybook.title = 'Ace Stories';
+// mybook.author = 'Aaron Griffin';
+// mybook.pages = '300';
+// mybook.read = 'yes';
+
+// console.log(mybook)
+
+
+
+
+
+
+
+
