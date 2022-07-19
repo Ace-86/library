af88221d4768bf41a06f6342cab1d7fe509b0109
Ace <kronicrespawn@gmail.com>
2022-07-18 21:35:18 -0500

    modal exits on submit

--- script.js
+++ script.js
@@ -10,11 +10,9 @@
         complete: true
     }
  ];
+
  // ----dom elements----
- 
- // let btn = document.getElementById("addBtn");
 $modalForm = document.getElementById('modalForm');
-// $form = document.querySelector('#contact-form');?
 $newBookBtn = document.querySelector('#addBtn');
 $submit = document.querySelector('#submit')
 $clearBtn = document.querySelector('#clearBtn');
@@ -31,32 +29,32 @@ $complete = document.querySelector('#complete');
      this.page = page;
      this.complete = complete;
     };
-    
-
-    
- 
-    
+        
     // ------modal------
     
+    //add button (modal popup)
     $newBookBtn.onclick = function() {
         $modalForm.style.display = "block";
     }
     
+    //modal exit button
     $exitModal.onclick = function () {
         $modalForm.style.display = "none";
     }
-
-    $submit.onclick = function(e) {
+//modal submit button
+    $submit.onclick = function() {
         // e.preventDefault(); 
         getUserInput();
-
+        $modalForm.style.display = "none";
     };
-// window.onclick = function(event) {
-//     if (event.target == $modalForm) {
-//         $modalForm.style.display = "none";
-//     }
-// }
-// ------modal----
+
+    // close modal when click outside modal
+    window.onclick = function(event) {
+        if (event.target == $modalForm) {
+            $modalForm.style.display = "none";
+        }
+    }
+
 
 
    const getUserInput = () => {
@@ -68,103 +66,6 @@ $complete = document.querySelector('#complete');
         console.log(newBook);
         library.push(newBook);
     };
-
-
-
-
-
-
-
-
-// ----form-----
-
-// function createBook(event) {
-//     event.preventDefault();
-//     const bookForm = new FormData(this);
-//     const entries = bookForm.entries();
-//     const data = Object.fromEntries(entries);
-//     console.log(data);
-// };
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function Book(title, author, pages, read) {
-//     this.title = title;
-//     this.author = author;
-//     this.pages = pages;
-//     // this.read = read;
-//     // this.info = function () {
-//     //      return 'The book title: ' + title + ' by ' + author + '. The book is ' + pages +' pages long. book completed: ' + read ;
-//     // }
-// }
-
-// Book.prototype.sayTitle = function () {
-//     console.log(this.title)
-// }
-
-// const book = {
-//     read : false,
-//     info :function () {
-//         return 'The book title: ' + title + ' by ' + author + '. The book is ' + pages +' pages long. book completed: ' + read ;
-//    }
-// }
-
-
-// const book1 = new Book('IT', 'Steven King', '978', 'no');
-// const book2 = new Book('Nice Guy', 'Dean Koontz', '648', 'yes')
-// const book3 = new Book('Danger Zone', 'Aiden Finnigan', '540', 'no')
-// const mybook = Object.create(book);
-// // const person = {
-// //     isHuman: false,
-// //     printIntroduction: function() {
-// //       console.log(`My name is ${this.name}. Am I human? ${this.isHuman}`);
-// //     }
-// //   };
-
-
-
-// //   const Ace = Object.create(person);
-
-// // Ace.name = 'Aaron';
-// // Ace.isHuman = true;
-
-// // Ace.printIntroduction();
-
-// mybook.title = 'Ace Stories';
-// mybook.author = 'Aaron Griffin';
-// mybook.pages = '300';
-// mybook.read = 'yes';
-
-// console.log(mybook)
-
-
 
 
 
